008e6e1f406c0047e3f9f8bc58ecb27b7dd544b9
Eric <eric.lee@dal,ca>
2018-02-10 11:14:21 -0400

    added some rules added a simple if/else statement to judge passwords - the textView.setText() is making my app crash?!?!? Although results do print in the system.

--- Validator.java
+++ Validator.java
@@ -5,23 +5,50 @@ package com.example.eric.password_validator;
  */
 
 import org.junit.Test;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import java.util.regex.Pattern;
+
 import static org.junit.Assert.*;
 
 public class Validator
 {
-    @Test
-    public void Validator
+    String pw = "";
+    String bad = "password";
+    String errResponse = "Invalid password";
+
+    public boolean Validator(String pw)
     {
-        String pw = "";
-        String bad = "password";
-        String errResponse = "Invalid password";
+
+        Pattern digits = Pattern.compile("[0-9]");
 
         if (pw.equalsIgnoreCase(bad)) //check if the password is password, invalid if yes
-        {System.out.println(errResponse);}
-        else if (pw.length()<8) //check if password is less than 8, invalid if yes
-        {System.out.println(errResponse);}
+        {
+            return false;
+        }
+        else if (pw.length() < 8) //check if password is less than 8, invalid if yes
+        {
+            return false;
+        }
+        else if (!digits.matcher(pw).find()) //check if password has a digit
+        {
+            return false;
+        }
         else //passed the other rules
-        {System.out.println("good");}
+        {
+            return true;
+        }
 
+
+    }
+    @Test
+    public void containDigit()
+    {
+
+        assertTrue(Validator("123qwE"));
+
+        assertFalse(Validator("Password"));
+        assertFalse(Validator("qwop0987"));
+        assertFalse(Validator("123qwE"));
     }
 }
